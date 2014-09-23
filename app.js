df99f3b8f7a1483d395f0aff829c2400b7abec56
Aaron Johnson <ajohnson13.aj@gmail.com>
2014-09-22 23:29:51 -0500

    app.js static edit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var http = require('http');
-var connect = require('connect');
+var connect = require('connect'), serveStatic = require('serve-static');
 var directory = 'public';
 var logger = require('morgan');
 var port = process.env.PORT;
 
 var app = connect()
   .use(logger(':remote-addr -> :method :url [:status]'))
-  .use(connect.static(directory));
+  .use(serveStatic(directory));
 
 http.createServer(app).listen(port, function(){
   console.log('Node server listening. Port: ' + port + ', Directory: ' + directory);
